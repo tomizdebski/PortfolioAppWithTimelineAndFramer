bc41514963915934e453712e31aab3e61b630322
tomizdebski <tomizdebski@gmail.com>
2024-01-08 20:10:05 +0100

    add_header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Tom | Personal Portfolio',
-  description: 'Tom is a software engineer based in Warsaw, PL with 2 years of experience.',
-}
+  title: "Tom | Personal Portfolio",
+  description:
+    "Tom is a software engineer based in Warsaw, PL with 2 years of experience.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950`}>
-        <div className="bg-[#fbe2e3] absolute top-[-1rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]">b</div>
-        <div className="bg-[#dbd7fb] absolute top-[-6rem] left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]">b</div>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className="bg-[#fbe2e3] absolute -z-10 top-[-1rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]">
+          
+        </div>
+        <div className="bg-[#dbd7fb] absolute -z-10 top-[-6rem] left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]">
+          
+        </div>
         {children}
-
-        </body>
+      </body>
     </html>
-  )
+  );
 }
